b4cf47877d300d0c011e858e61cbafd749c8e6d5
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2024-05-24 22:10:55 +0100

    move

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
-﻿namespace MyProject;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator;
 class Program
 {
  static void Main(string[] args)
  {
   Console.WriteLine("Hello, World!");
+  Console.WriteLine("Please enter a number");
+
+  Console
  }
 }
